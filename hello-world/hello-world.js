eb739f496691b3db0db436da537a7c94766c5aff
Nayema Nasir <nayema.nasir89@gmail.com>
2017-11-23 16:36:56 -0500

    Changes variables in accorance with ES6

--- hello-world/hello-world.js
+++ hello-world/hello-world.js
@@ -1,10 +1,4 @@
-//
-// This is a stub file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at hello-world.spec.js--that should give you some hints about what is
-// expected here.
-
-var HelloWorld = function () {};
+const HelloWorld = function () {}
 
 HelloWorld.prototype.hello = function () {
     return 'Hello, World!'
--- hello-world/hello-world.spec.js
+++ hello-world/hello-world.spec.js
@@ -1,7 +1,7 @@
-var HelloWorld = require('./hello-world');
+const HelloWorld = require('./hello-world')
 
 describe('Hello World', function () {
-  var helloWorld = new HelloWorld();
+  const helloWorld = new HelloWorld()
 
   it('says hello world', function () {
     expect(helloWorld.hello()).toEqual('Hello, World!');

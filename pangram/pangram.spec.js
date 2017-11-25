055cc6fe0496487bc9b7c07d65f34a84f092c05c
Nayema Nasir <nayema.nasir89@gmail.com>
2017-11-24 22:03:10 -0500

    Rewrites testing description to follow Arrange-Act-Assert principle and adds coverage for all numbers and all CAPs

--- pangram/pangram.spec.js
+++ pangram/pangram.spec.js
@@ -1,48 +1,102 @@
 const Pangram = require('./pangram')
 
 describe('Pangram()', function ()  {
-  it('empty sentence', function () {
-    const pangram = new Pangram('')
-    expect(pangram.isPangram()).toBe(false);
-  });
+  it('returns false when the sentence is empty', function () {
+    const input = ''
+    const pangram = new Pangram(input)
 
-  xit('pangram with only lower case', function ()  {
-    const pangram = new Pangram('the quick brown fox jumps over the lazy dog')
-    expect(pangram.isPangram()).toBe(true);
-  });
+    const result = pangram.isPangram()
 
-  xit("missing character 'x'", function ()  {
-    const pangram = new Pangram('a quick movement of the enemy will jeopardize five gunboats');
-    expect(pangram.isPangram()).toBe(false);
-  });
+    expect(result).toEqual(false)
+  })
 
-  xit("another missing character 'x'", function () {
-    const pangram = new Pangram('the quick brown fish jumps over the lazy dog');
-    expect(pangram.isPangram()).toBe(false);
-  });
+  it('returns false when the sentence has only numbers', function () {
+    const input = '5465465463'
+    const pangram = new Pangram(input)
 
-  xit('pangram with underscores', function () {
-    const pangram = new Pangram('the_quick_brown_fox_jumps_over_the_lazy_dog');
-    expect(pangram.isPangram()).toBe(true);
-  });
+    const result = pangram.isPangram()
 
-  xit('pangram with numbers', function () {
-    const pangram = new Pangram('the 1 quick brown fox jumps over the 2 lazy dogs');
-    expect(pangram.isPangram()).toBe(true);
-  });
+    expect(result).toEqual(false)
+  })
 
-  xit('missing letters replaced by numbers', function () {
-    const pangram = new Pangram('7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog');
-    expect(pangram.isPangram()).toBe(false);
-  });
+  it('returns true when the sentence has only lower case letters', function ()  {
+    const input = 'the quick brown fox jumps over the lazy dog'
+    const pangram = new Pangram(input)
 
-  xit('pangram with mixed case and punctuation', function ()  {
-    const pangram = new Pangram('"Five quacking Zephyrs jolt my wax bed."');
-    expect(pangram.isPangram()).toBe(true);
-  });
+    const result = pangram.isPangram()
 
-  xit('pangram with non-ascii characters', function ()  {
-    const pangram = new Pangram('Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.');
-    expect(pangram.isPangram()).toBe(true);
-  });
-});
+    expect(result).toEqual(true)
+  })
+
+  it('returns true when the sentence has only upper case letters', function ()  {
+    const input = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(true)
+  })
+
+  it("returns false when the sentence is missing character 'x'", function ()  {
+    const input = 'a quick movement of the enemy will jeopardize five gunboats'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(false)
+  })
+
+  it("returns false if the sentence is missing another character 'x'", function () {
+    const input = 'the quick brown fish jumps over the lazy dog'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(false)
+  })
+
+  it('returns true if the sentence contains underscores', function () {
+    const input = 'the_quick_brown_fox_jumps_over_the_lazy_dog'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(true)
+  })
+
+  it('returns true if the sentence contains numbers', function () {
+    const input = 'the 1 quick brown fox jumps over the 2 lazy dogs'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(true)
+  })
+
+  it('returns false when missing letters are replaced by numbers', function () {
+    const input = '7h3 qu1ck brown fox jumps ov3r 7h3 lazy dog'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(false)
+  })
+
+  it('returns true when the sentence includes mixed case and punctuation', function ()  {
+    const input = '"Five quacking Zephyrs jolt my wax bed."'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(true)
+  })
+
+  it('returns true if the sentence includes non-ascii characters', function ()  {
+    const input = 'Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich.'
+    const pangram = new Pangram(input)
+
+    const result = pangram.isPangram()
+
+    expect(result).toEqual(true)
+  })
+})

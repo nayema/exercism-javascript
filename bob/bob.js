cdec28678aaecac7fa0b349113d77000f07e6e44
Nayema Nasir <nayema.nasir89@gmail.com>
2017-12-01 18:10:25 -0500

    Reduces repeated return statements

--- bob/bob.js
+++ bob/bob.js
@@ -1,12 +1,9 @@
 const Bob = function () {}
 
 Bob.prototype.hey = function (input) {
-  if (input.charAt(input.length - 1) === '!' && !input.includes('Let\'s')) {
-    return 'Whoa, chill out!'
-  } else if ((input === input.toUpperCase()) &&
-    (input !== input.toLowerCase())) {
-    return 'Whoa, chill out!'
-  } else if ((input === input.toUpperCase()) && input.includes('?')) {
+  if ((input.charAt(input.length - 1) === '!' && !input.includes('Let\'s')) ||
+    ((input === input.toUpperCase()) && (input !== input.toLowerCase())) ||
+    ((input === input.toUpperCase()) && input.includes('?'))) {
     return 'Whoa, chill out!'
   } else if (!input.match(/\S/)) {
     return 'Fine. Be that way!'
